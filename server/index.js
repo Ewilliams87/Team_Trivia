b1e2be34285ff98cb84776847c530826c42e2a33
Eric <Ewilliams1677@gmail.com>
2025-10-18 12:03:42 -0500

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -82,4 +82,4 @@ app.get('/questions', async (req, res) => {
 
 
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
